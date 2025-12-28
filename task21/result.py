8c6b13f3078c2a0e85603d2c9c7eb46fa4d089a6
mileoa <ezkimozzzzz@gmail.com>
2025-12-28 21:19:59 +1000

    add anser

---
+++ task21/result.py
@@ -0,0 +1,70 @@
+# Наследование реализации (implementation inheritance)
+class CaseInsensitiveDict(dict):
+
+    def __setitem__(self, key, value):
+        if isinstance(key, str):
+            key = key.lower()
+        super().__setitem__(key, value)
+
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            key = key.lower()
+        return super().__getitem__(key)
+
+    def __contains__(self, key):
+        if isinstance(key, str):
+            key = key.lower()
+        return super().__contains__(key)
+
+
+# Льготное наследование (facility inheritance)
+import copy
+import pickle
+from abc import abstractmethod, ABC
+
+
+class General(object):
+
+    def copy_form(self, object: "General") -> None:
+        for key, value in self.__dict__.items():
+            setattr(self, key, value)
+
+    def deep_copy(self, object: "General"):
+        object_copy = copy.deepcopy(self)
+        for key, value in object_copy.__dict__.items():
+            setattr(self, key, value)
+
+    def clone(self) -> "General":
+        return copy.deepcopy(self)
+
+    # Сравнение делается через магический метод __eq__ и уже реализован в object
+
+    def serialize(self) -> bytes:
+        return pickle.dumps(self)
+
+    def deserialize(self, object: bytes) -> None:
+        obj = pickle.loads(object)
+        self.__dict__.update(obj.__dict__)
+
+    # печать (наглядное представление содержимого объекта в текстовом формате); Реализовано в object.__repr__
+
+    def is_eq_type(self, object: "General") -> bool:
+        return isinstance(self, type(object))
+
+    # получение реального типа объекта (непосредственного класса, экземпляром которого он был создан).
+    def get_type(self):
+        return type(self)
+
+
+class Any(General):
+    pass
+
+
+class Animal(ABC, General):
+
+    def __init__(self, name: str):
+        self.name: str = name
+
+    @abstractmethod
+    def make_sound(self) -> str:
+        pass
